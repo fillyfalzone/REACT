5ac3c377e089870af34c77c2191a39a42c05cd5c
Arnaud Kameni <bekaman100@gmail.com>
2024-02-25 03:33:32 +0100

    edit comments

--- listmanage/src/components/Fruit.jsx
+++ listmanage/src/components/Fruit.jsx
@@ -1,12 +1,16 @@
 export default function Fruit ({fruitInfo, onClick}) {
-    // state
 
-    // const fruitInfo = props.fruitInfo;
-    // const onFruitDelete = props.onFruitDelete;
+    /**
+        *state
+    */  
 
-    // comportements
+    /**
+        *Comportements  
+    */  
 
-    // affichage (render)
+        /**
+        *affichage (render)
+    */  
     return (
         <li className='py-2 w-25 d-flex justify-content-between align-items-center'>  
             {fruitInfo.name} 
